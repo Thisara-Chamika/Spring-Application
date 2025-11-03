d68d46d3455c94663ad182d3f345932c4d1fcea5
Thisara-Chamika <thisarachamika2003@gmail.com>
2025-11-03 15:50:16 +0530

    return a customer object

--- src/main/java/edu/icet/ecom/controller/CustomerController.java
+++ src/main/java/edu/icet/ecom/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package edu.icet.ecom.controller;
 
+import edu.icet.ecom.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,17 @@ public class CustomerController {
     @GetMapping("/age")
     public Integer getAge(){
         return 22;
+    }
+
+    @GetMapping("/get-customer")
+    public Customer getCustomer(){
+        return new Customer(
+            "1",
+            "Thisara",
+            "Panadura",
+            20000.0
+        );
+
     }
 
 }
